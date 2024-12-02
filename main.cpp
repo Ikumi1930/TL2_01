ab15f16b3380a4240bb95d17c81651cad7ff2ba1
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2024-12-02 16:08:42 +0900

    02_03終わり

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() 
+int main(int argc, char* argv[])
 {
-	printf("Hello,world!\n");
+	//argc‚Ì”‚¾‚¯ŒJ‚è•Ô‚·
+	for (int i = 0; i < argc; i++) {
+		//•¶š—ñargv‚Ìi”Ô‚ğ•\¦
+		printf(argv[i]);
+		//‰üs
+		printf("\n");
+	}
 
-
+	
 	system("pause");
+
 	return 0;
 }

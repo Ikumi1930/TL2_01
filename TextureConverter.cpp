864ce4f484d16786806eb28ee3a9aed8600ec085
Ikumi1930 <115620514+Ikumi1930@users.noreply.github.com>
2024-12-03 22:30:57 +0900

    02_06終わり

--- TextureConverter.cpp
+++ TextureConverter.cpp
@@ -101,14 +101,14 @@ void TextureConverter::SaveDDSTextureToFile() {
 			metadata_ = scratchImage_.GetMetadata();
 		}
 
-		//圧縮形式に変換
+		//**//圧縮形式に変換
 		ScratchImage converted;
 		result = Compress(scratchImage_.GetImages(), scratchImage_.GetImageCount(), metadata_, DXGI_FORMAT_BC7_UNORM_SRGB, TEX_COMPRESS_BC7_QUICK | TEX_COMPRESS_SRGB_OUT | TEX_COMPRESS_PARALLEL, 1.0f, converted);
 		if (SUCCEEDED(result)) {
 			scratchImage_ = std::move(converted);
 			metadata_ = scratchImage_.GetMetadata();
 		}
-
+		
 
 		//読み込んだテクスチャをSRGBとして扱う
 		metadata_.format = MakeSRGB(metadata_.format);
